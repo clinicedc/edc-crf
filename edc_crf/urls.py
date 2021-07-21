20e06db891e26ae22e66f3088dddeceb1d0e87b7
erikvw <ew2789@gmail.com>
2021-07-21 11:23:31 -0500

    update urls

--- edc_crf/urls.py
+++ edc_crf/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls.conf import path
 from django.views.generic import RedirectView
 
 app_name = "edc_crf"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/admin/edc_crf/"), name="home_url"),
+    path("", RedirectView.as_view(url=f"/{app_name}/admin/"), name="home_url"),
 ]
