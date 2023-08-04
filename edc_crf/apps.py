9461cb61d92cb3f16ccc2c6e77a2f69cf2e9892e
erikvw <ew2789@gmail.com>
2023-08-04 15:05:11 -0500

    move model/signal to subfolder

--- edc_crf/apps.py
+++ edc_crf/apps.py
@@ -7,5 +7,5 @@ class AppConfig(DjangoAppConfig):
     has_exportable_data = True
     include_in_administration_section = True
 
-    def ready(self):
-        from .signals import update_crf_status_post_save  # NOQA
+    # def ready(self):
+    #     from .signals import update_crf_status_post_save  # NOQA
