7cefb398da3a142c592fab467ed3e23e83ed10ba
erikvw <ew2789@gmail.com>
2023-10-09 14:14:14 -0500

    update paths from edc-visit-schedule, setup

--- edc_crf/tests/tests/test_crf_status.py
+++ edc_crf/tests/tests/test_crf_status.py
@@ -6,7 +6,7 @@ from edc_constants.constants import COMPLETE, INCOMPLETE
 from edc_facility import import_holidays
 from edc_reference import site_reference_configs
 from edc_utils import get_utcnow
-from edc_visit_schedule import site_visit_schedules
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_tracking.constants import SCHEDULED
 from edc_visit_tracking.tests.helper import Helper
 from visit_schedule_app.consents import v1_consent
