e548ab6bdc6b93593f90d1ec174d571c1cf42f4a
erikvw <ew2789@gmail.com>
2023-10-30 21:40:12 -0500

    remove edc-reference as a dependency

--- runtests.py
+++ runtests.py
@@ -37,7 +37,6 @@ project_settings = DefaultTestSettings(
         "edc_metadata.apps.AppConfig",
         "edc_notification.apps.AppConfig",
         "edc_protocol.apps.AppConfig",
-        "edc_reference.apps.AppConfig",
         "edc_registration.apps.AppConfig",
         "edc_sites.apps.AppConfig",
         "edc_timepoint.apps.AppConfig",
