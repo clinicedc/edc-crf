66bcf43ad990bf31b2f299cf2436b23f9f78333e
erikvw <ew2789@gmail.com>
2024-11-04 05:54:59 -0600

    do not include on administration page

--- edc_crf/apps.py
+++ edc_crf/apps.py
@@ -5,4 +5,4 @@ class AppConfig(DjangoAppConfig):
     name = "edc_crf"
     verbose_name = "Edc CRF"
     has_exportable_data = True
-    include_in_administration_section = True
+    include_in_administration_section = False
