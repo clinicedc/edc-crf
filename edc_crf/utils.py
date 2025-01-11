f2321f2bc5e22bb6f3b0d2df7b836a60fee4cea8
erikvw <ew2789@gmail.com>
2025-01-10 19:07:45 -0600

    HasCrfChecker

--- edc_crf/utils.py
+++ edc_crf/utils.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.apps import apps as django_apps
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
 from edc_model import model_exists_or_raise
@@ -21,3 +21,42 @@ def raise_if_crf_does_not_exist(subject_visit, model: str) -> None:
             verbose_name=verbose_name,
         )
         raise forms.ValidationError(err_message)
+
+
+class HasCrfChecker:
+    def __init__(
+        self,
+        app_name: str,
+        subject_visit_model_cls,
+    ):
+        self.app_name = app_name
+        self.subject_visit_model_cls = subject_visit_model_cls
+        self.models = []
+        for model in django_apps.get_app_config(self.app_name).get_models():
+            if model._meta.label_lower.split(".")[1].startswith("historical"):
+                continue
+            if "subject_visit" in [f.name for f in model._meta.get_fields()]:
+                self.models.append(model)
+
+    def has_crfs(
+        self,
+        subject_identifier,
+        visit_code: str,
+        visit_code_sequence: int,
+    ):
+        subject_visit = None
+        try:
+            subject_visit = self.subject_visit_model_cls.objects.get(
+                subject_identifier=subject_identifier,
+                visit_code=visit_code,
+                visit_code_sequence=visit_code_sequence,
+            )
+        except ObjectDoesNotExist:
+            pass
+        except MultipleObjectsReturned:
+            raise MultipleObjectsReturned("Specify the visit_code and visit_code_sequence")
+        else:
+            for model in self.models:
+                if model.objects.filter(subject_visit=subject_visit).exists():
+                    return subject_visit, True
+        return subject_visit, False
